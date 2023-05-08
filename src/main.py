e2f4dd8583483cd9e00a1e369f0a93cc55a8f045
William Merfalen <wmerfalen@gmail.com>
2023-05-08 17:32:58 -0600

    feat: ghetto tcp scanner

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+for host in sys.argv[1:]:
+    print(host,"scanning")
+    for port in range(1,65535):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.0250)
+            s.connect((host,port))
+            s.close()
+            print(host,"open",port)
+        except ConnectionRefusedError:
+            s.close()
+        except Exception:
+            s.close()
+    print(host,"done")
+
